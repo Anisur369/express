8af1e00dcf11f86b7fa86d8e76a2cf2d488f5b76
Anisur369 <anisurrahman57136@gmail.com>
2025-08-21 02:57:15 +0600

    commit

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 
 const app = express();
-const admin = express();
+const adminRoute = express.Router();
 
-admin.get('/', (req, res) => {
-  res.send('Welcome to the admin panel!');
+adminRoute.get('/dashboard', (req, res) => {
+  console.log("req.url:==", req.url);
+  console.log("req.baseUrl:==", req.baseUrl);
+  console.log("req.originalURL:==", req.originalUrl);
+  console.log("req.path:==", req.path);
+  console.log("req.hostname:==", req.hostname);
+  console.log("req.ip:==", req.ip);
+  console.log("req.method:==", req.method);
+  console.log("req.protocol:==", req.protocol);
+  console.log("req.query:==", req.query);
+  console.log("req.body:==", req.body);
+  console.log("req.params.id:==", req.params.id);
+  console.log("req.params:==", req.params);
+  res.send('Welcome to the admin dashboard!');
 });
 
-app.param('id', (req, res, next, id) => {
-  console.log(`User ID: ${id}`);
-  const user= {
-    userid:id,
-    name: 'Bangladesh'
-  }
-  req.userDetails = user;
-  next();
+app.use('/admin', adminRoute);
+
+app.get('/user/:id', (req, res) => {
+  console.log("req.url:==", req.url);
+  console.log("req.baseUrl:==", req.baseUrl);
+  console.log("req.originalURL:==", req.originalUrl);
+  console.log("req.path:==", req.path);
+  console.log("req.hostname:==", req.hostname);
+  console.log("req.ip:==", req.ip);
+  console.log("req.method:==", req.method);
+  console.log("req.protocol:==", req.protocol);
+  console.log("req.query:==", req.query);
+  console.log("req.body:==", req.body);
+  console.log("req.params.id:==", req.params.id);
+  console.log("req.params:==", req.params);
+  res.send('Welcome to the user panel!');
 });
 
-app.all('/user/:id', (req, res) => {
-  res.send(`Welcome to the application home page, ${req.userDetails.name}!`);
-});
-
-app.use('/admin', admin);
 
 app.listen(3000, () => {
   console.log('Server is running on port http://localhost:3000/');
