a509df19f7e438c3ca30f0929b266930f07eabfd
Anisur369 <anisurrahman57136@gmail.com>
2025-08-22 02:21:53 +0600

    commit

--- handle.js
+++ handle.js
@@ -1,6 +1,9 @@
 const handle = (req, res) => {
-  console.log(req.app.locals.title);
-  res.send('This is home page');
+  req.app.get('/gest',(req,res) => {
+    console.log('i am a gest');
+    res.send('This is /gest page');
+  });
+  res.send('This is handle page');
 };
 
 module.exports = handle;
--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const handle = require('./handle.js');
 
 const app = express();
-const adminRoute = express.Router();
-
-adminRoute.get('/dashboard', (req, res) => {
-  console.log("req.url:==", req.url);
-  console.log("req.baseUrl:==", req.baseUrl);
-  console.log("req.originalURL:==", req.originalUrl);
-  console.log("req.path:==", req.path);
-  console.log("req.hostname:==", req.hostname);
-  console.log("req.ip:==", req.ip);
-  console.log("req.method:==", req.method);
-  console.log("req.protocol:==", req.protocol);
-  console.log("req.query:==", req.query);
-  console.log("req.body:==", req.body);
-  console.log("req.params.id:==", req.params.id);
-  console.log("req.params:==", req.params);
-  res.send('Welcome to the admin dashboard!');
-});
-
-app.use('/admin', adminRoute);
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/user/:id', (req, res) => {
-  console.log("req.url:==", req.url);
-  console.log("req.baseUrl:==", req.baseUrl);
-  console.log("req.originalURL:==", req.originalUrl);
-  console.log("req.path:==", req.path);
-  console.log("req.hostname:==", req.hostname);
-  console.log("req.ip:==", req.ip);
-  console.log("req.method:==", req.method);
-  console.log("req.protocol:==", req.protocol);
-  console.log("req.query:==", req.query);
-  console.log("req.body:==", req.body);
-  console.log("req.params.id:==", req.params.id);
-  console.log("req.params:==", req.params);
+  // console.log(req.body);
+  // console.log(req.cookies);
+  console.log(req.secure);  
   res.send('Welcome to the user panel!');
 });
+app.get('/', handle);
 
+app.post('/user/:id', (req, res) => {
+  console.log(req.cookies);
+  res.send('User created successfully!');
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port http://localhost:3000/');
 });
   
+
+
+//npm i cookie-parser
