f8cc83e7f162a7c367efe407e10e8a708d663999
Anisur369 <anisurrahman57136@gmail.com>
2025-08-30 06:57:30 +0600

    jani na

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const adminRouter = require('./adminRouter');
-const publicRouter = require('./publicRouter');
+// const adminRouter = require('./adminRouter');
+// const publicRouter = require('./publicRouter');
 
 const app = express();
 
-app.use('/', publicRouter);
-app.use('/admin', adminRouter);
+// app.use('/', publicRouter);
+// app.use('/admin', adminRouter);
+
+app.get('/', (req, res) => {
+  throw new Error('Something went wrong');
+});
 
 
 app.listen(3000, () => {
