85c26a1eabdd2502d3925829d489a8fd49dfae77
Anisur369 <anisurrahman57136@gmail.com>
2025-08-30 07:02:03 +0600

    delete

--- handle.js
+++
@@ -1,9 +0,0 @@
-const handle = (req, res) => {
-  req.app.get('/gest',(req,res) => {
-    console.log('i am a gest');
-    res.send('This is /gest page');
-  });
-  res.send('This is handle page');
-};
-
-module.exports = handle;
